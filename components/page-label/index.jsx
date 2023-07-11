8e769fa7e5f1d6ca974ddadcf833b10695ee6b1b
sailor-lin <1401820861@qq.com>
2023-07-11 18:13:13 +0800

    feat: 添加pageLabel组件；

---
+++ components/page-label/index.jsx
@@ -0,0 +1,135 @@
+import Icon from '../icon';
+import { historyBack } from '../_utils/history.js';
+import { defineComponent, ref } from 'vue';
+
+const PageLabel = defineComponent({
+  name: 'CPageLabel',
+  props: {
+    // 标题
+    title: {
+      type: String,
+      default: "标题",
+    },
+    // 提示
+    tips: {
+      type: String,
+      default: "",
+    },
+    // 图标
+    icon: {
+      type: String,
+      default: "",
+    },
+    // 显示顶部导航栏
+    showHeader: {
+      type: Boolean,
+      default: false,
+    },
+    // 默认返回路径
+    back: {
+      type: String,
+      default: '',
+    },
+    // 文档链接
+    documentLink: String,
+    // 文档提示文本
+    documentText: {
+      type: String,
+      default: '了解更多'
+    },
+  },
+  setup(props, { attrs, slots, emit, expose }) {
+    
+    const breadcrumbRef = ref();
+    const methods = {
+      // 默认返回方法
+      backHandler() {
+        // 若有自定义返回方法，则调用该方法
+        if(props.onBack) {
+          props.onBack();
+          return;
+        }
+        // 默认返回方法，若无可返回路径，则跳转到指定返回路径
+        historyBack(props.back);
+      }
+    };
+
+    expose({
+      back: methods.back,
+    });
+
+    return () => {
+      const customSlots = {
+        ...slots,
+        breadcrumb: () => {
+          return slots.header
+            ? slots.header()
+            : props.showHeader
+              ? <div class="cyber-page-label-header">
+                  <Icon
+                    icon="cyber-fanhui"
+                    isSvg
+                    size="21"
+                    class="cyber-back-icon"
+                    onClick={methods.backHandler}
+                  ></Icon>
+                  { slots.breadcrumb?.() }
+                </div>
+              : undefined;
+        },
+        icon: () => {
+          return slots.icon
+            ? slots.icon?.()
+            : <Icon isSvg icon={props.icon} size="64"></Icon>
+        },
+        title: () => {
+          return <div class="cyber-page-label-title">
+            {
+              slots.title
+                ? slots.title?.()
+                : props.title
+            }
+          </div>
+        },
+        tips: () => {
+          return slots.tips || props.tips
+            ? <div class="cyber-page-label-tips">
+                { slots.tips
+                    ? slots.tips()
+                    : props.tips
+                }
+                {
+                  props.documentLink
+                    ? <a href={props.documentLink} class="document_link">{ props.documentText }</a>
+                    : undefined
+                }
+              </div>
+            : undefined
+        },
+      }
+      return (
+        <div class="cyber-page-label">
+          { customSlots.breadcrumb() }
+          <div class="cyber-page-label-body">
+            <div class="cyber-page-label-body-left">
+              <div class="title-icon">
+                { customSlots.icon() }
+              </div>
+              <div class="cyber-page-label-content">
+                { customSlots.title() }
+                { customSlots.tips() }
+              </div>
+            </div>
+            { customSlots.right?.() }
+          </div>
+        </div>
+      );
+    }
+  },
+});
+
+PageLabel.install = (app) => {
+  app.component(PageLabel.name, PageLabel);
+};
+
+export default PageLabel;
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,22 @@
 <template>
+  <div style="padding: 24px 24px 0;">
+    <c-page-label
+      title="标题"
+      tips="提示。"
+      icon="cyber-xianshimima"
+      document-link="javascript:;"
+      showHeader
+    >
+      <template #breadcrumb>
+        <a-breadcrumb>
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+          <a-breadcrumb-item><a href="">Application List</a></a-breadcrumb-item>
+          <a-breadcrumb-item>An Application</a-breadcrumb-item>
+        </a-breadcrumb>
+      </template>
+    </c-page-label>
+  </div>
   <!-- 步骤条 -->
   <Icon></Icon>
   <!-- 开关 -->
@@ -23,6 +41,7 @@ import CollapseForm from './modules/CollapseForm.vue';
 import Table from './modules/Table.vue';
 import Select from './modules/Select.vue';
 import Radio from './modules/Radio.vue';
+import { Breadcrumb as ABreadcrumb, BreadcrumbItem as ABreadcrumbItem } from 'ant-design-vue';
 </script>
 
 <style lang="less" scoped>
